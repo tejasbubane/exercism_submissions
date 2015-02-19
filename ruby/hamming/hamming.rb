17c4641c24be9ff02c1d39357b52eff3c005721d
Tejas Bubane <tejas.bubane@infibeam.net>
2015-02-19 09:57:38 +0530

    Ruby: Hamming: Use count instead of inject.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,8 +2,8 @@ class Hamming
   def self.compute(strand_1, strand_2)
     zipped_strands = strand_1.chars.zip(strand_2.chars)
 
-    zipped_strands.inject(0) do |distance, (this_char, other_char)|
-      this_char != other_char ? distance + 1 : distance
-    end
+    zipped_strands.select do |this_char, other_char|
+      this_char != other_char
+    end.count
   end
 end
