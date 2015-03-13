121d6c62f83e890615342dd73ab9b9eec17a25f4
Tejas Bubane <tejasbubane@gmail.com>
2015-03-13 19:27:44 +0530

    Ruby: Leap: Iteration #4: another approach.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,23 +1,21 @@
-module Year
+class Year
   def self.leap?(year)
-    LeapChecker.new(year).leap?
+    new(year).leap?
   end
 
-  class LeapChecker
-    def initialize(year)
-      @year = year
-    end
+  def initialize(year)
+    @year = year
+  end
 
-    def leap?
-      divisible_by?(400) || divisible_by?(4) && !divisible_by?(100)
-    end
+  def leap?
+    divisible_by?(400) || divisible_by?(4) && !divisible_by?(100)
+  end
 
-    private
+  private
 
-    attr_reader :year
+  attr_reader :year
 
-    def divisible_by?(num)
-      year % num == 0
-    end
+  def divisible_by?(num)
+    year % num == 0
   end
 end
