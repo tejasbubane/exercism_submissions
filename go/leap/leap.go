68df06748942bd17cffbe7b81e0202cc946b8dc3
Tejas Bubane <tejasbubane@gmail.com>
2015-03-17 19:38:13 +0530

    Go: Leap: First submission.

---
+++ go/leap/leap.go
@@ -0,0 +1,13 @@
+package leap
+
+func IsLeapYear(year int) bool {
+	if IsDivisibleBy(year, 4) && !IsDivisibleBy(year, 100) || IsDivisibleBy(year, 400) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsDivisibleBy(dividend int, divisor int) bool {
+	return dividend%divisor == 0
+}
