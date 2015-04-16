747a1c6f1d3dd622edb7ff5940cc66a0fd7983d4
Tejas Bubane <tejasbubane@gmail.com>
2015-04-16 15:43:36 +0530

    Update with ArgumentError for new tests.

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,34 @@
+module Complement
+  COMPLEMENT = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
+  def self.of_dna(dna)
+    DNA.new(dna).complement
+  end
+
+  def self.of_rna(rna)
+    RNA.new(rna).complement
+  end
+
+  DNA = Struct.new(:sequence) do
+    def complement
+      sequence.chars.map do |nucleotide|
+        raise ArgumentError unless COMPLEMENT[nucleotide]
+        COMPLEMENT[nucleotide]
+      end.join
+    end
+  end
+
+  RNA = Struct.new(:sequence) do
+    def complement
+      sequence.chars.map do |nucleotide|
+        raise ArgumentError unless COMPLEMENT.key(nucleotide)
+        COMPLEMENT.key(nucleotide)
+      end.join
+    end
+  end
+end
