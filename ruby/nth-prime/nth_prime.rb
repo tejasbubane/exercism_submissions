d3477fe670f7fbb82b3c1495f246354523d48953
Tejas Bubane <tejasbubane@gmail.com>
2015-05-13 23:13:07 +0530

    Ruby: Nth Prime: First iteration.

---
+++ ruby/nth-prime/nth_prime.rb
@@ -0,0 +1,27 @@
+class Prime
+  def self.nth(n)
+    raise ArgumentError if n == 0
+
+    count = 0
+    (1..Float::INFINITY).each do |i|
+      count += 1 if prime?(i)
+      return i if count == n
+    end
+  end
+
+  private
+
+  def prime?(n)
+    return true if n == 2
+    return false if n == 1 || n.even?
+
+    has_factors?(n)
+  end
+
+  def has_factors?(n)
+    (3..Math.sqrt(n).to_i).each do |divisor|
+      return false if n % divisor == 0
+    end
+    return true
+  end
+end
