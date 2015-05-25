6badb7adafcf7d7ed331229df22752901ef76e4a
Tejas Bubane <tejasbubane@gmail.com>
2015-05-26 00:14:29 +0530

    Elixir: Bob

---
+++ elixir/bob/hello.exs
@@ -0,0 +1,14 @@
+defmodule Teenager do
+  def hey(input) do
+    cond do
+			String.strip(input) == "" -> "Fine. Be that way!"
+			String.ends_with?(input, "?") -> "Sure."
+			String.upcase(input) == input && !only_numbers(input) -> "Whoa, chill out!"
+			true -> "Whatever."
+    end
+  end
+
+	defp only_numbers(input) do
+		input =~ ~r/^[0-9,.\s]*$/
+	end
+end
