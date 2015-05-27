2f085d83288777ff75454c0c53fdefa56b91db9a
Tejas Bubane <tejasbubane@gmail.com>
2015-05-27 13:32:47 +0530

    Ruby: Binary: First iteration.

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,25 @@
+class Binary
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def to_decimal
+    return 0 unless binary
+
+    digits.each_with_index.inject(0) do |result, (digit, index)|
+      result + digit * 2**index
+    end
+  end
+
+  private
+
+  def digits
+    input.chars.reverse.map(&:to_i)
+  end
+
+  def binary
+    input =~ /^(0*1*)*$/
+  end
+end
