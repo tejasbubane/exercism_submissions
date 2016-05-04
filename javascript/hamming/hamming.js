f96a49ef9064c1cc4300492a6bb64564860e8082
Tejas Bubane <tejasbubane@gmail.com>
2016-05-04 19:00:02 +0530

    Javascript: Hamming

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,23 @@
+var Hamming = function() {
+  return {
+    compute: function(strand_1, strand_2) {
+      if(strand_1.length !== strand_2.length) {
+        throw new Error('DNA strands must be of equal length.');
+      }
+
+      return this._getDifference(strand_1, strand_2);
+    },
+
+    _getDifference: function(strand_1, strand_2) {
+      var difference = 0;
+      for(var i=0; i < strand_1.length; i++) {
+        if (strand_1[i] !== strand_2[i]) {
+          difference++;
+        }
+      }
+      return difference;
+    }
+  }
+}
+
+module.exports = Hamming;
