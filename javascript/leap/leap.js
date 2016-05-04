7203cc33d0367d9476463996dc8b071dff87358d
Tejas Bubane <tejasbubane@gmail.com>
2016-05-04 18:26:54 +0530

    Complete the leap exercise in javascript

---
+++ javascript/leap/leap.js
@@ -0,0 +1,15 @@
+var Year = function (year) {
+  return {
+    year: year,
+
+    isLeap: function() {
+      return this._every(400) || (!this._every(100) && this._every(4));
+    },
+
+    _every: function(duration) {
+      return this.year % duration === 0;
+    }
+  }
+}
+
+module.exports = Year
