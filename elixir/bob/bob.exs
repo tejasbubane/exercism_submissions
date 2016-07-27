bbd2e3596549232a96603b182743fbb109d4293e
Tejas Bubane <tejasbubane@gmail.com>
2016-07-27 20:25:17 +0530

    Elixir: Bob: Third iteration

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,32 @@
 defmodule Teenager do
+
   def hey(input) do
     cond do
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.ends_with?(input, "?") -> "Sure."
-      String.upcase(input) == input && !only_numbers(input) -> "Whoa, chill out!"
-      true -> "Whatever."
+      silence?(input) ->
+        "Fine. Be that way!"
+      shouting?(input) && !numbers?(input) ->
+        "Whoa, chill out!"
+      question?(input) ->
+        "Sure."
+      true ->
+        "Whatever."
     end
   end
 
-  defp only_numbers(input) do
-    input =~ ~r/^[0-9,.\s]*$/
+  defp shouting?(input) do
+    String.upcase(input) == input
   end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp silence?(input) do
+    String.strip(input) == ""
+  end
+
+  defp numbers?(input) do
+    String.match?(input, ~r/^[0-9\s,\?!]+$/)
+  end
+
 end
