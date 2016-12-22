a4ce2e297f6fd54ec5fd822598241611582e6bb7
Tejas Bubane <tejasbubane@gmail.com>
2016-12-22 12:45:05 +0530

    Ecmascript: Word count

---
+++ ecmascript/word-count/word-count.js
@@ -0,0 +1,12 @@
+export default class WordCount {
+  count(sentence) {
+    let words = sentence.split(/\s+/).filter((word) => word !== "");
+
+    return words.reduce((counts, word) => {
+      word = word.toLowerCase();
+      if(!(typeof(counts[word]) === 'number')) { counts[word] = 0; } // initialize
+      counts[word] += 1;
+      return counts;
+    }, {})
+  }
+}
