6cdc1b016a6bf22685c98a4acbe0634f7fbc7313
Tejas Bubane <tejasbubane@gmail.com>
2019-01-17 19:17:06 +0530

    Haskell: Sum of Multiples: Fourth Iteration
    Update problem spec to newer version - handle case of factor `0`.

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -5,6 +5,7 @@ import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit =
-  sum . nub . map fst . filter divisible $ liftM2 (,) range factors
+  sum . nub . map fst . filter divisible $ liftM2 (,) range factors'
   where range = [1..(limit-1)]
         divisible = ((== 0) . uncurry mod)
+        factors' = filter (/= 0) factors
