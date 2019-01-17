d06edeeb6776f4baed08a10e14d908c8445f7581
Tejas Bubane <tejasbubane@gmail.com>
2019-01-17 19:37:43 +0530

    Haskell: Sum of Multiples: Fifth Iteration
    Based on mentor feedback:
    
    * switching `filter` to any, like in the first iteration, would
      eliminate the need for expensive `O(N^2)` `nub`ing.
    * `rem` is faster than `mod`.

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -4,8 +4,7 @@ import Control.Monad (liftM2)
 import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit =
-  sum . nub . map fst . filter divisible $ liftM2 (,) range factors'
+sumOfMultiples factors limit = sum $ filter factor range
   where range = [1..(limit-1)]
-        divisible = ((== 0) . uncurry mod)
+        factor = (\x -> any ((== 0) . rem x) factors')
         factors' = filter (/= 0) factors
