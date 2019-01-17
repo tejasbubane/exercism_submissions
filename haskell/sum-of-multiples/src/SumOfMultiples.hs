978fb808a531d95564cb9cbe010b77b6b7c29e66
Tejas Bubane <tejasbubane@gmail.com>
2019-01-17 19:55:57 +0530

    Haskell: Sum of Multiples: Sixth Iteration
    Based on mentor feedback:
    
    * I'd inline the `range` (which is also used a function name, causing miscoloring by the pretty printer)
    * see also the `hlint` tool output for notational hints or http://pointfree.io for an alternative notation
    * name `hasFactor` or similar might be more informative than `factor`

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -4,7 +4,6 @@ import Control.Monad (liftM2)
 import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ filter factor range
-  where range = [1..(limit-1)]
-        factor = (\x -> any ((== 0) . rem x) factors')
+sumOfMultiples factors limit = sum $ filter hasFactor [1..(limit-1)]
+  where hasFactor x = any ((== 0) . rem x) factors'
         factors' = filter (/= 0) factors
