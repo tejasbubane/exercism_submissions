ba3561fa6b95402ca277912f2ea21369bfffab6a
Tejas Bubane <tejasbubane@gmail.com>
2019-01-17 19:12:59 +0530

    Haskell: Sum of multiples: Third iteration
    Small cleanup - reduce line length

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -5,5 +5,6 @@ import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit =
-  sum . nub . map fst . filter ((== 0) . uncurry mod) $ liftM2 (,) range factors
+  sum . nub . map fst . filter divisible $ liftM2 (,) range factors
   where range = [1..(limit-1)]
+        divisible = ((== 0) . uncurry mod)
