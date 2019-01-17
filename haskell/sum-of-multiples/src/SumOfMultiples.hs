c3433876c781c865f51485965ca703bf76811290
Tejas Bubane <tejasbubane@gmail.com>
2019-01-17 20:34:22 +0530

    Haskell: Sum of multiples: Remove unused imports

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,8 +1,5 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Control.Monad (liftM2)
-import Data.List (nub)
-
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum $ filter hasFactor [1..(limit-1)]
   where hasFactor x = any ((== 0) . rem x) factors'
