d1cb4aabcba3dedd8f7ff436e43ebe56e0449e1f
Tejas Bubane <tejasbubane@gmail.com>
2019-01-19 23:10:43 +0530

    Haskell: Nucleotide Counts: Third iteration

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,24 +1,18 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map, insertWith, fromList)
+import Text.Read (readEither)
+import Control.Monad (foldM)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
 type NucleotideCounts = Map Nucleotide Int
 
-makeNucleotide :: Char -> Either String Nucleotide
-makeNucleotide 'A' = Right A
-makeNucleotide 'C' = Right C
-makeNucleotide 'G' = Right G
-makeNucleotide 'T' = Right T
-makeNucleotide _   = Left "Invalid nucleotide"
-
-countDNA :: Either String NucleotideCounts -> Char -> Either String NucleotideCounts
-countDNA e@(Left _) _ = e
-countDNA (Right m) x =
-  case makeNucleotide x of
+countDNA :: NucleotideCounts -> Char -> Either String NucleotideCounts
+countDNA m x =
+  case readEither [x] of
     Left e  -> Left e
     Right n -> Right $ insertWith (+) n 1 m
 
 nucleotideCounts :: String -> Either String NucleotideCounts
-nucleotideCounts xs = foldl countDNA (Right $ fromList [(A, 0), (C, 0), (G, 0), (T, 0)]) xs
+nucleotideCounts xs = foldM countDNA (fromList [(A, 0), (C, 0), (G, 0), (T, 0)]) xs
