a9a8f22120039e4b90e0bcffb1da07c60ef21638
Tejas Bubane <tejasbubane@gmail.com>
2019-12-24 23:57:28 +0530

    Ruby: High Scores: Second iteration

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -6,15 +6,15 @@ class HighScores
   end
 
   def latest
-    scores[-1]
+    @latest ||= scores.last
   end
 
   def personal_best
-    scores.max
+    @personal_best ||= scores.max
   end
 
   def personal_top_three
-    scores.sort.reverse.first(3)
+    @personal_top_three ||= scores.max(3)
   end
 
   def latest_is_personal_best?
