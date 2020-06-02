b900cc6e6c8f3fb01f03b3d17e1151df0c3b589f
Tejas Bubane <tejasbubane@gmail.com>
2020-06-02 23:21:58 +0530

    Elixir: Triangle: first iteration

---
+++ elixir/triangle/lib/triangle.ex
@@ -0,0 +1,27 @@
+defmodule Triangle do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0 do
+    {:error, "all side lengths must be positive"}
+  end
+
+  def kind(a, b, c) when a + b <= c or a + c <= b or b + c <= a do
+    {:error, "side lengths violate triangle inequality"}
+  end
+
+  def kind(a, b, c) do
+    {:ok, triangle_type(a, b, c)}
+  end
+
+  defp triangle_type(a, b, c) do
+    cond do
+      a == b && b == c -> :equilateral
+      a == b || b == c || c == a -> :isosceles
+      true -> :scalene
+    end
+  end
+end
