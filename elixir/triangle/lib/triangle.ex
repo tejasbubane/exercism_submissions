ff30f5cbdfa8ebb429274ec5cc46c139e0ba75c9
Tejas Bubane <tejasbubane@gmail.com>
2025-01-20 11:29:25 +0400

    Elixir: Triangle: Third iteration using guard clauses

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -1,24 +1,21 @@
 defmodule Triangle do
-  def kind(a, b, c) do
-    case validate(a, b, c) do
-      {:ok, sides} -> {:ok, find_kind(sides)}
-      {:error, message} -> {:error, message}
-    end
+  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0 do
+    {:error, "all side lengths must be positive"}
   end
 
-  defp validate(a, b, c) do
-    cond do
-      a <= 0 or b <= 0 or c <= 0 -> {:error, "all side lengths must be positive"}
-      a + b < c or b + c < a or a + c < b -> {:error, "side lengths violate triangle inequality"}
-      true -> {:ok, {a, b, c}}
-    end
+  def kind(a, b, c) when a + b < c or b + c < a or a + c < b do
+    {:error, "side lengths violate triangle inequality"}
   end
 
-  defp find_kind({a, b, c}) do
-    cond do
-      a == b and b == c -> :equilateral
-      a == b or b == c or a == c -> :isosceles
-      true -> :scalene
-    end
+  def kind(a, b, c) when a == b and b == c do
+    {:ok, :equilateral}
+  end
+
+  def kind(a, b, c) when a == b or b == c or a == c do
+    {:ok, :isosceles}
+  end
+
+  def kind(_, _, _) do
+    {:ok, :scalene}
   end
 end
